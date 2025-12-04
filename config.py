2ab1a25f72358981dea9f19c319a57d2f0d17835
darknagi <narutohindidubbed16@gmail.com>
2025-12-04 15:43:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,5 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID"))
-
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = os.getenv("DB_NAME", "default")
